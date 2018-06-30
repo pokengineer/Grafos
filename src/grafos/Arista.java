c58016b788fc28f59a77e500d29452bd5be70afc
pokengineer <vfacuu@gmail.com>
2018-06-30 19:24:04 -0300

    prim

--- src/grafos/Arista.java
+++ src/grafos/Arista.java
@@ -1,10 +1,16 @@
 package grafos;
 
-public class Arista {
+public class Arista implements Comparable<Arista> {
 	private int no;
 	private int nd;
+	private int value;
 
-	
+	public Arista(int no, int nd, int value) {
+		this.no = no;
+		this.nd = nd;
+		this.value = value;
+	}
+
 	public Arista(int i, int j) {
 		this.no = i;
 		this.nd = j;
@@ -24,6 +30,15 @@ public class Arista {
 
 	public void setNd(int nd) {
 		this.nd = nd;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	@Override
+	public int compareTo(Arista o) {
+		return this.value - o.value;
 	}
 
 }
--- src/grafos/Procesadora.java
+++ src/grafos/Procesadora.java
@@ -9,19 +9,21 @@ public class Procesadora {
 	private int[][] mat;
 
 	public Procesadora(int n) {
-		this.mat = new int[n-1][n-1];
+		this.mat = new int[n - 1][n - 1];
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat.length; j++) {
 				if (Math.random() < 0.6) {
-					mat[i][j] = (int) (Math.random() * 10)+1;
+					mat[i][j] = (int) (Math.random() * 10) + 1;
 				} else
 					mat[i][j] = INFINITO;
 			}
 		}
 	}
-	
+
 	public Procesadora() {
-		this.mat = new int[][] {{0,5,INFINITO,INFINITO,2},{INFINITO,0,7,9,INFINITO},{4,INFINITO,0,INFINITO,INFINITO},{INFINITO,INFINITO,INFINITO,0,5},{2,INFINITO,INFINITO,INFINITO,0}};
+		this.mat = new int[][] { { 0, 5, INFINITO, INFINITO, 2 }, { INFINITO, 0, 7, 9, INFINITO },
+				{ 4, INFINITO, 0, INFINITO, INFINITO }, { INFINITO, INFINITO, INFINITO, 0, 5 },
+				{ 2, INFINITO, INFINITO, INFINITO, 0 } };
 	}
 
 	public void mostrarNodos() {
@@ -38,10 +40,10 @@ public class Procesadora {
 	public static void main(String[] args) {
 		Procesadora p = new Procesadora();
 		p.mostrarNodos();
-		int[] caca = p.dijkstra(1);
-	
-		for(int i=0; i< caca.length ; i++)
-			System.out.println(caca[i]);
+//		int[] caca = p.dijkstra(1);
+//		for (int i = 0; i < caca.length; i++)
+//			System.out.println(caca[i]);
+		p.prim(2);
 	}
 
 	/**
@@ -58,7 +60,7 @@ public class Procesadora {
 
 		// pongo en dist las distancias que ya tengo y pongo en infinito lo que me falta
 		for (int i = 0; i < dist.length; i++) {
-			if ( (mat[nodo][i] != 0) &&  (nodo != i) ) {
+			if ((mat[nodo][i] != 0) && (nodo != i)) {
 				dist[i] = mat[nodo][i];
 			} else {
 				dist[i] = INFINITO;
@@ -94,6 +96,73 @@ public class Procesadora {
 		}
 
 		return dist;
+	}
+
+	/**
+	 * Consigue el arbol abarcativo de minimo valor (min spanning tree).
+	 */
+	public void kuskal() {
+		ArrayList<Arista> listaAristas = new ArrayList<Arista>();
+
+		// agrego las aristas a la lista
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if ((mat[i][j] != INFINITO) && (i != j)) {
+					listaAristas.add(new Arista(i, j, mat[i][j]));
+				}
+			}
+		}
+
+		// ordeno la lista por valor
+		Collections.sort(listaAristas);
+
+		// Esta incompleto porque el tema del union find me da paja, veo de implementar
+		// prim
+	}
+
+	/**
+	 * Consigue el arbol abarcativo de minimo valor (min spanning tree).
+	 * 
+	 * @param nodoInicial
+	 */
+	public void prim(int nodoInicial) {
+		PriorityQueue<Arista> aristasRestantes = new PriorityQueue<Arista>(); // para bajar la complejidad computacional
+		ArrayList<Arista> aristasDelArbol = new ArrayList<Arista>();
+		boolean[] nodosRecorridos = new boolean[mat.length];
+		Arista aux;
+
+		// cargo las aristas del nodo actual
+		for (int i = 0; i < mat.length; i++) {
+			if ((mat[nodoInicial][i] != INFINITO) && (nodoInicial != i)) {
+				aristasRestantes.add(new Arista(nodoInicial, i, mat[nodoInicial][i]));
+			}
+		}
+
+		nodosRecorridos[nodoInicial] = true;
+
+		// recorro mietras queden aristas por recorrer
+		while (!aristasRestantes.isEmpty()) {
+			// saco la cabeza
+			aux = aristasRestantes.poll();
+
+			// si es un nodo que no recorri
+			if (!nodosRecorridos[aux.getNd()]) {
+				nodosRecorridos[aux.getNd()] = true;
+				aristasDelArbol.add(aux);
+
+				for (int i = 0; i < mat.length; i++) {
+					if ((mat[aux.getNd()][i] != INFINITO) && (aux.getNd() != i)) {
+						aristasRestantes.add(new Arista(aux.getNd(), i, mat[aux.getNd()][i]));
+					}
+				}
+			}
+
+		}
+
+		// quiero ver si quedo bien armado
+		for (Arista item : aristasDelArbol) {
+			System.out.println(item.getNo() + " " + item.getNd() + " " + item.getValue());
+		}
 	}
 
 }
